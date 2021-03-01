1c5009abb267e2fdf5a3f9760a34d934cdfd5eb0
Vahid Kiani <vdkkia2004@yahoo.com>
2021-03-02 00:44:47 +0100

    Convert interestOverTime to ES6

--- src/core/controllers/trendController.js
+++ src/core/controllers/trendController.js
@@ -1,79 +1,78 @@
 const logger = require("../logger");
+// const googleTrends = require("google-trends-api");
 const googleTrends = require("../trendsAPI");
 const helper = require("../helpers");
 const trends = require("../../models/trend");
 const slayer = require("slayer");
 
+// const HttpsProxyAgent = require("https-proxy-agent");
+// const proxyAgent = new HttpsProxyAgent("127.0.0.1:9050");
+
 // 100 is the most popularity
 const interestOverTime = async (keyword, period, agent) => {
-  return new Promise((resolve, reject) => {
-    googleTrends
-      .interestOverTime({
-        keyword: keyword.includes(",") ? keyword.split(",") : keyword,
-        startTime: helper.data.getProcessedDate(period),
-        // timezone: 60,
-        // category: 814, //Currencies & Foreign Exchange
-        granularTimeResolution: true,
-        // property: "youtube", //  enumerated string ['images', 'news', 'youtube' or 'froogle']
-        // endTime: helper.data.getProcessedDate("3d"),
-        // geo: 'US', //defaults to worldwide
-        agent,
-      })
-      .then((res) => {
-        resolve(JSON.parse(res).default.timelineData);
-      })
-      .catch((err) => {
-        if (err.requestBody && err.requestBody.includes("Error 429 (Too Many Requests)!!")) {
-          logger.error(agent.proxy.host + " is banned...");
-          return resolve("Banned");
-        }
-        logger.error("Error: " + err + " keyword " + keyword + "  Host: " + agent.proxy.host);
-        resolve("Proxy down...");
-      });
-  });
-};
-
-const dailyTrends = async (period) => {
+  // period =< 4 ==> timeFrame: 1 minute
+  // period > 4 ==> timeFrame: 8 minuts
+  let res;
   try {
-    const res = await googleTrends.dailyTrends({
-      trendDate: helper.data.getProcessedDate(period),
-      geo: "US",
-    });
-    return JSON.parse(res);
-  } catch (e) {
-    return e;
-  }
-};
-
-const interestByRegion = async (keyword, period) => {
-  try {
-    const res = await googleTrends.interestByRegion({
-      keyword,
-      resolution: "COUNTRY",
+    res = await googleTrends.interestOverTime({
+      keyword: keyword.includes(",") ? keyword.split(",") : keyword,
       startTime: helper.data.getProcessedDate(period),
+      // timezone: 60,
+      // category: 814, //Currencies & Foreign Exchange
+      granularTimeResolution: true,
+      // property: "youtube", //  enumerated string ['images', 'news', 'youtube' or 'froogle']
+      // endTime: helper.data.getProcessedDate("3d"),
+      // geo: 'US', //defaults to worldwide
+      agent,
     });
-    return JSON.parse(res);
+    return JSON.parse(res).default.timelineData;
   } catch (e) {
-    return e;
+    logger.error("Error: " + e + " keyword " + keyword + "  Host: " + agent.proxy.host);
+    return null;
   }
 };
 
-const relatedQueries = async (keyword, period) => {
-  try {
-    const res = await googleTrends.relatedQueries({
-      keyword,
-      startTime: helper.data.getProcessedDate(period),
-      // category: 814,//Currencies & Foreign Exchange
-      // endTime:
-      // geo:
-    });
-    return JSON.parse(res);
-  } catch (e) {
-    return e;
-  }
-};
+// const dailyTrends = async (period) => {
+//   try {
+//     const res = await googleTrends.dailyTrends({
+//       trendDate: helper.data.getProcessedDate(period),
+//       geo: "US",
+//     });
+//     return JSON.parse(res);
+//   } catch (e) {
+//     return e;
+//   }
+// };
 
-googleTrends.relatedQueries({ keyword: "Westminster Dog Show" });
+// const interestByRegion = async (keyword, period) => {
+//   try {
+//     const res = await googleTrends.interestByRegion({
+//       keyword,
+//       resolution: "COUNTRY",
+//       startTime: helper.data.getProcessedDate(period),
+//     });
+//     return JSON.parse(res);
+//   } catch (e) {
+//     return e;
+//   }
+// };
+
+// const relatedQueries = async (keyword, period) => {
+//   try {
+//     const res = await googleTrends.relatedQueries({
+//       keyword,
+//       startTime: helper.data.getProcessedDate(period),
+//       // category: 814,//Currencies & Foreign Exchange
+//       // endTime:
+//       // geo:
+//     });
+//     return JSON.parse(res);
+//   } catch (e) {
+//     return e;
+//   }
+// };
+
+// googleTrends.relatedQueries({ keyword: "Westminster Dog Show" });
 
 // /// Shots is the number of 4-hour data
 // /// e.g. shot == 8 >> 8 x 4-hour data (each with 1 hour overlap) >> 24 hours useful data
@@ -119,24 +118,24 @@ googleTrends.relatedQueries({ keyword: "Westminster Dog Show" });
 //   return output;
 // };
 
-const detectSpike = async (data) => {
-  const spikes = await slayer({ minPeakDistance: 100, minPeakHeight: 0 })
-    .y((item) => item.value)
-    .fromArray(data);
-  return spikes;
-};
+// const detectSpike = async (data) => {
+//   const spikes = await slayer({ minPeakDistance: 100, minPeakHeight: 0 })
+//     .y((item) => item.value)
+//     .fromArray(data);
+//   return spikes;
+// };
 
-const getReference = (data) => {
-  for (let i = data.length - 1; i > 0; i--) {
-    if (data[i].value != 0) return data[i];
-  }
-};
+// const getReference = (data) => {
+//   for (let i = data.length - 1; i > 0; i--) {
+//     if (data[i].value != 0) return data[i];
+//   }
+// };
 
 module.exports = {
   interestOverTime,
-  dailyTrends,
-  interestByRegion,
-  relatedQueries,
+  // dailyTrends,
+  // interestByRegion,
+  // relatedQueries,
   // getNormalizeData,
-  detectSpike,
+  // detectSpike,
 };
