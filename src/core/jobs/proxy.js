69586c692c522fdd3917de47e65f106d4226e336
Vahid Kiani <vdkkia2004@yahoo.com>
2021-03-01 18:18:57 +0100

    Add shuffling available proxies before acquire

--- src/core/jobs/proxy.js
+++ src/core/jobs/proxy.js
@@ -1,3 +1,4 @@
+const logger = require("../logger");
 const ProxyAgent = require("proxy-agent");
 const hosts = require("./hosts");
 let proxies = [];
@@ -8,7 +9,6 @@ const proxyPool = {
     proxies = hosts.map((x, i) => ({
       host: x,
       number: i + 1,
-      retry: 0,
       status: "ready",
       updateTime: new Date(),
     }));
@@ -24,47 +24,30 @@ const proxyPool = {
   acquire: (number) => {
     return pool[number].proxy;
   },
-  replace: (number) => {
+  replace: (number, result) => {
     const failedProxy = proxies.find((p) => p.number == pool[number].number);
-    if (failedProxy.retry >= 20) {
-      failedProxy.retry = 0;
+    if (result == "Banned") {
       failedProxy.status = "banned";
     } else {
-      failedProxy.retry++;
       failedProxy.status = "ready";
-      // console.log("retry", failedProxy.retry, number);
     }
-    const item = proxies.find((p) => p.status == "ready");
-    if (!item) return "error";
+    const readyProxies = proxies.filter((x) => x.status == "ready");
+    const item = readyProxies[Math.round(Math.random() * readyProxies.length)];
+    if (!item) return false;
     pool[number] = { proxy: new ProxyAgent(item.host), number: item.number };
     item.status = "active";
     item.updateTime = new Date();
     return pool[number].proxy;
   },
   activate: () => {
-    console.log("PROXY POOL REFRESH....")
+    logger.info("PROXY POOL REFRESH....");
     proxies.forEach((proxy) => {
-      if (proxy.status == "banned" && new Date() - proxy.updateTime > 3 * 3600 * 1000) {
+      if (proxy.status == "banned" && new Date() - proxy.updateTime > 2 * 3600 * 1000) {
         proxy.status = "ready";
         proxy.updateTime = new Date();
       }
     });
   },
 };
-
-// (async () => {
-//   proxyPool.init(1);
-//   const agent = new ProxyAgent("http://pcvpttjl-dest:w0kgh6nois6f@45.130.255.183:7205/")// proxyPool.acquire(0);
-
-//   //   const proxyUri = "http://wupziwtp-dest:c2kw3rgv3znp@209.127.191.180:9279"
-//   //   const agent = new ProxyAgent(proxyUri);
-//   const trendController = require("../controllers/trendController");
-//   const res = await trendController.interestOverTime("NSBT coin,Phantasma,soul coin,Umbrella Network,umb coin", "30m", null);
-//   console.log(res);
-
-//   // for (let i = 0; i < 200; i++) {
-//   //   t(i);
-//   // }
-// })();
 
 module.exports = proxyPool;
