2f2bf82cd74fd9311f33cbccb1019ca9c2eaccfb
Farid Khaheshi <farid.khaheshi@gmail.com>
2021-03-06 10:17:59 +0330

    adds the basic infrastructure to the docker-compose file

--- src/core/jobs/sleep.js
+++
@@ -1,8 +0,0 @@
-module.exports = (second) => {
-  const ms = second || Math.floor(Math.random() * 5) + 2;
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, ms * 1000);
-  });
-};
