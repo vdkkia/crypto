169f09bd28dffd13c2ab9be3180d9da34c1a2dab
Farid Khaheshi <farid.khaheshi@gmail.com>
2021-03-06 12:37:18 +0330

    adds backoffice to document changes to the database schema and handle migrations

---
+++ backoffice/index.js
@@ -0,0 +1,23 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(process.env.POSTGRES_URI, {
+  dialect: "postgres",
+  protocol: "postgres",
+  // dialectOptions: {
+  //   ssl: {
+  //     require: true,
+  //     rejectUnauthorized: false,
+  //   },
+  // },
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+console.log("Hello");
---
+++ backoffice/migrations/20210306085823-add_timescaledb_extension.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.query(
+      "CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;"
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.query("DROP EXTENSION timescaledb;");
+  },
+};
