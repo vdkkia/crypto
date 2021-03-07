f8fc8d5790c5ae0efb4d6149c8782027e6d2e69b
Vahid Kiani <vdkkia2004@yahoo.com>
2021-03-07 06:54:45 +0100

    Fix jump detector

--- src/core/signalDetector.js
+++ src/core/signalDetector.js
@@ -1,22 +1,22 @@
 const detector = async (data, logger) => {
   let mostRecent;
   try {
-    if (data && data.length) {
+    if (data && data.length > 0) {
       mostRecent = Number(data.slice(-1)[0].value[0]);
       logger.info(`mostRecent is ${mostRecent}`);
       const values = data.filter((x) => x.value).map((x) => x.value[0]);
-      if (values.length) {
+      if (values.length > 0) {
         const avg = values.reduce((t, i) => t + i) / values.length;
         logger.warn(`Average is ${avg}`);
         logger.warn(`mostRecent is ${mostRecent}`);
-        if (mostRecent >= 2 * avg) {
+        if (avg > 0 && mostRecent >= 2 * avg) {
           logger.info("=================");
           logger.info("Jump detected!!!");
           logger.info("=================");
         }
       }
     } else {
-      logger.info("Data is incomplete.");
+      logger.warn("Data is incomplete.");
     }
   } catch (err) {
     logger.error("Error" + err);
