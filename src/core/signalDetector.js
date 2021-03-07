8afbad2551b8d34f1197774ed8058c96ded92068
Vahid Kiani <vdkkia2004@yahoo.com>
2021-03-07 12:45:56 +0100

    Update logic based upon group req

--- src/core/signalDetector.js
+++ src/core/signalDetector.js
@@ -1,20 +1,18 @@
-const detector = async (data, logger) => {
-  let mostRecent;
+const detector = async (data, keywords, logger) => {
   try {
-    if (data && data.length > 0) {
-      mostRecent = Number(data.slice(-1)[0].value[0]);
-      logger.info(`mostRecent is ${mostRecent}`);
-      const values = data.filter((x) => x.value).map((x) => x.value[0]);
-      if (values.length > 0) {
-        const avg = values.reduce((t, i) => t + i) / values.length;
-        logger.warn(`Average is ${avg}`);
-        logger.warn(`mostRecent is ${mostRecent}`);
-        if (avg > 0 && mostRecent >= 2 * avg) {
-          logger.info("=================");
-          logger.info("Jump detected!!!");
-          logger.info("=================");
+    if (data && data.length > 1) {
+      const lastData = data.pop();
+      const restData = data;
+      lastData.value.forEach((val, i) => {
+        // Calculate average of item i in restData
+        const avg = restData.map((x) => x.value[i]).reduce((t, j) => t + j) / restData.length;
+        logger.warn(`Coin: ${keywords[i]} Average is ${avg}, value is: ${val}`);
+        if (avg > 0 && val > avg * 5) {
+          logger.warn("=======================================");
+          logger.info(`Jump detected!!!, coin: ${keywords[i]}`);
+          logger.warn("=======================================");
         }
-      }
+      });
     } else {
       logger.warn("Data is incomplete.");
     }
