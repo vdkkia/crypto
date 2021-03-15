3202f6b5b18da1aa101a150910cbfc4c8e6360ca
Farid Khaheshi <cryptadmin@ip-172-31-22-83.us-east-2.compute.internal>
2021-03-15 19:28:52 +0000

    handles api errors when sending requests to normalizer

--- scraper/src/services/normalization/send-data-for-normalization.js
+++ scraper/src/services/normalization/send-data-for-normalization.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const sendDataForNormalization = async (payload) => {
-  const { data } = axios.post(
+  const { data } = await axios.post(
     `${process.env.NORMALIZER_API_BASE_URI}/api/data`,
     payload
   );
