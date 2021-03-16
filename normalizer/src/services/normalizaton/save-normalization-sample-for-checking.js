e66726df34f8e2509a9bdb48a69b11761fcf08f3
Farid Khaheshi <farid.khaheshi@gmail.com>
2021-03-16 13:27:40 +0330

    changes the sampling period to 30 minutes

--- normalizer/src/services/normalizaton/save-normalization-sample-for-checking.js
+++ normalizer/src/services/normalizaton/save-normalization-sample-for-checking.js
@@ -1,12 +1,12 @@
 const logger = require("../logger");
 const Normalization = require("./Normalization");
 
-const FIVE_MINS = 5 * 60;
+const SAMPLING_SECONDS = 30 * 60;
 
 const saveNormalizationSampleForChecking = async (sample) => {
   try {
     const reportTime = sample.timelineData[sample.timelineData.length - 1].time;
-    if (reportTime % FIVE_MINS === 0) {
+    if (reportTime % SAMPLING_SECONDS === 0) {
       await Normalization.create(sample);
       logger.info(`saved normalization sample to mongodb.`);
     }
