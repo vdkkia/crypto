5238a9822fc4eee9f018d98b2dd96a8ef380aefe
Vahid Kiani <vdkkia2004@yahoo.com>
2021-03-18 00:04:51 +0100

    Add moving_average table

--- db/init/01_add-timescaledb.sql
+++ db/init/01_add-timescaledb.sql
@@ -1,23 +1,30 @@
 CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
 
-CREATE TABLE cointerests (
-  REPORT_KEY TEXT KEY,
-  INTEREST_PLAIN INTEGER,
-  INTEREST DOUBLE PRECISION,
-  KEYWORD TEXT,
-  TIMESTAMP TEXT,
-  REPORT_TIME BIGINT,
-  BATCH_NO INTEGER,
-  HAS_DATA BOOLEAN,
-  FORMATTED_AXIS_TIME TEXT,
-  FORMATTED_TIME TEXT,
-  FORMATTED_VALUE TEXT
+CREATE TABLE "cointerests"(
+  "REPORT_KEY" text primary key,
+  "INTEREST_PLAIN" integer,
+  "INTEREST" double precision,
+  "KEYWORD" text,
+  "TIMESTAMP" text,
+  "REPORT_TIME" bigint,
+  "BATCH_NO" integer,
+  "HAS_DATA" boolean,
+  "FORMATTED_AXIS_TIME" text,
+  "FORMATTED_TIME" text,
+  "FORMATTED_VALUE" text
 );
 
 CREATE TABLE moving_average (
-  KEYWORD TEXT,
-  RATIO DOUBLE PRECISION,
-  CALC_TIME TEXT,
+  "KEYWORD" text,
+  "RATIO" double precision,
+  "CALC_TIME" text
 );
 
-SELECT create_hypertable('cointerests', 'REPORT_TIME');
+SELECT create_hypertable('cointerests', 'REPORT_TIME', chunk_time_interval => 604800000);
+
+CREATE TABLE moving_average (
+  "KEYWORD" text,
+  "RATIO" double precision,
+  "CALC_TIME" text
+);
+
